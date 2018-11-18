04da1270366dc0c8f18a2f95020cd0a85810e1ca
DevHunter <32465888+DevHunterYZ@users.noreply.github.com>
2018-11-18 22:33:59 +0300

    Update BinaryCNN.py

--- BinaryCNN.py
+++ BinaryCNN.py
@@ -26,11 +26,11 @@ shear_range = 0.2,
 zoom_range = 0.2,
 horizontal_flip = True)
 test_datagen = ImageDataGenerator(rescale = 1./255)
-training_set = train_datagen.flow_from_directory('C:/Users/user/Desktop/weather_database/training',
+training_set = train_datagen.flow_from_directory('weather_database/training',
 target_size = (64, 64),
 batch_size = 32,
 class_mode = 'binary')
-test_set = test_datagen.flow_from_directory('C:/Users/user/Desktop/weather_database/test',
+test_set = test_datagen.flow_from_directory('weather_database/test',
 target_size = (64, 64),
 batch_size = 32,
 class_mode = 'binary')
